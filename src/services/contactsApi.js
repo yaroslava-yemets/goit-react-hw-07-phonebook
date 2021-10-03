ec69afd5acd1f130b783d4d453c32d7935c9b45f
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-10-04 00:19:20 +0300

    run build and deployed on github

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -3,20 +3,16 @@ import axios from "axios";
 axios.defaults.baseURL = 'http://localhost:3000';
 
 export async function fetchContacts() {
-    console.log('fetch contacts');
     const { data } = await axios.get('/contacts');
-    console.log(data);
     return data;
 };
 
 export async function addContact(contact) {
-    console.log('add contact');
     const { data } = await axios.post('/contacts', contact);
     return data;
 };
 
 export async function deleteContact(id) {
-    console.log('delete contact');
     const { data } = await axios.delete(`/contacts/${id}`)
     return data;
 };
