874400e00f166f855c89aca8b3c0079475ba00d6
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-10-04 00:05:46 +0300

    added prettier, husky and lintstage

--- src/redux/contactForm/contacts-selector.js
+++ src/redux/contactForm/contacts-selector.js
@@ -1,20 +1,24 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.loading;
 export const getFilter = state => state.contacts.filter;
-export const getVisibleContacts = state => {
-    const allContacts = getContacts(state);
-    const filter = getFilter(state);
-    const lowerCasedFilter = filter.toLocaleLowerCase();
+export const getVisibleContacts = createSelector(
+    [getContacts, getFilter], 
+    (allContacts, filter) => {
+        const lowerCasedFilter = filter.toLocaleLowerCase();
+        return allContacts.filter(contact => 
+            contact.name.toLocaleLowerCase().includes(lowerCasedFilter));
+    },
+);
 
-    return allContacts.filter(contact => 
-        contact.name.toLocaleLowerCase().includes(lowerCasedFilter));
-};
+// =====  getVisibleContacts without memoization
 
-// const getVisibleContacts = (allContacts, filter) => {
+// export const getVisibleContacts = state => {
+//     const allContacts = getContacts(state);
+//     const filter = getFilter(state);
 //     const lowerCasedFilter = filter.toLocaleLowerCase();
-//     return allContacts.filter(contact => contact.name.toLocaleLowerCase().includes(lowerCasedFilter))
-// };
 
-// const mapStateToProps = ({ contacts: { items, filter } }) => ({
-//     contacts: getVisibleContacts(items, filter) 
-// });
+//     return allContacts.filter(contact => 
+//         contact.name.toLocaleLowerCase().includes(lowerCasedFilter));
+// };
